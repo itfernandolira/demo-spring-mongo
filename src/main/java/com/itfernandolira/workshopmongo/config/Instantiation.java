6559944717a549acb96d9462c801106a7cbea047
itfernandolira <it.fernandolira@gmail.com>
2021-03-06 10:50:12 +0000

    Author with DTO

--- src/main/java/com/itfernandolira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/itfernandolira/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.itfernandolira.workshopmongo.domain.Post;
 import com.itfernandolira.workshopmongo.domain.User;
+import com.itfernandolira.workshopmongo.dto.AuthorDTO;
 import com.itfernandolira.workshopmongo.repository.PostRepository;
 import com.itfernandolira.workshopmongo.repository.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1=new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para o Porto. Abraço!",maria);
-		Post post2=new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Dia perfeito hoje!",maria);
+		Post post1=new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para o Porto. Abraço!",new AuthorDTO(maria));
+		Post post2=new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Dia perfeito hoje!",new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
